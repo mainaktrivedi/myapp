de8db77437f3c4f66c03f6ec8d7d399abc3b71e4
mainak trivedi <mainak_trivedi@yahoo.com>
2016-09-17 19:02:01 -0700

    dependent deletion

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,7 +1,7 @@
 class Host < ActiveRecord::Base
 
 	belongs_to :org
-	has_many   :events
+	has_many   :events, dependent: :delete_all
 
 	validates :name, presence: true, uniqueness: true, length: { in: 2..50 }
 	validates :org, presence: true
--- app/models/org.rb
+++ app/models/org.rb
@@ -1,7 +1,7 @@
 class Org < ActiveRecord::Base
 
 
-	has_many		:hosts
+	has_many		:hosts, dependent: :destroy
 	has_many		:events, through: :hosts
 
 	validates :name, presence: true, uniqueness: true, length: { in: 2..10 }
